7deec6a5daf2ca3c814c2c12817cb73b1b397a48
soyoung6488 <soyoung6488@ajou.ac.kr>
2016-10-04 17:30:53 +0900

    sss

--- 운영체제과제1/src/운영체제과제1/merge_thread.java
+++ 운영체제과제1/src/운영체제과제1/merge_thread.java
@@ -1,20 +1,21 @@
 package 운영체제과제1;
 
 public class merge_thread implements Runnable{
-	private int[] sorted_string1;
-	private int[] sorted_string2;
+	private int[] sorted_number1;
+	private int[] sorted_number2;
 	
-	public merge_thread(int[] sorted_string1, int[] sorted_string2) {
+	public merge_thread(int[] sorted_number1, int[] sorted_number2) {
 		super();
-		this.sorted_string1 = sorted_string1;
-		this.sorted_string2 = sorted_string2;
+		this.sorted_number1 = sorted_number1;
+		this.sorted_number2 = sorted_number2;
 	}
 
 	@Override
 	public void run() {
 		int i,j;
-		for(i=0,j=0;i<sorted_string1.length || j<sorted_string2.length;){
-			if(sorted_string1[i]<sorted_string2[j]){
+		
+		for(i=0,j=0;i<sorted_number1.length || j<sorted_number2.length;){
+			if(sorted_number1[i]<sorted_number2[j]){
 				
 			}
 		}
