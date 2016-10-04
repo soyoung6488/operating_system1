6943e332cb6f60f80bd3465bcd071d7c33acaff2
soyoung6488 <soyoung6488@ajou.ac.kr>
2016-10-04 23:01:03 +0900

    으아ㅏ아 제발 자바로 하게 해주세요

--- 운영체제과제1/src/운영체제과제1/merge_thread.java
+++ 운영체제과제1/src/운영체제과제1/merge_thread.java
@@ -12,11 +12,26 @@ public class merge_thread implements Runnable{
 
 	@Override
 	public void run() {
-		int i,j;
-		
-		for(i=0,j=0;i<sorted_number1.length || j<sorted_number2.length;){
+		int i,j,k;
+		int[] merged_number = null;
+		for(i=0,j=0,k=0;i<sorted_number1.length || j<sorted_number2.length;k++){
 			if(sorted_number1[i]<sorted_number2[j]){
-				
+				merged_number[k]=sorted_number1[i];
+				i++;
+			}
+			else{
+				merged_number[k]=sorted_number2[j];
+				j++;
+			}
+		}
+		if(i==sorted_number1.length){
+			for(;j<sorted_number2.length;j++){
+				merged_number[k]=sorted_number2[j];
+			}
+		}
+		else{
+			for(;i<sorted_number1.length;i++){
+				merged_number[k]=sorted_number1[i];
 			}
 		}
 	}
